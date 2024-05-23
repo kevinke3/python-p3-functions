11d0e8a26ed140dbaedee64158c3389067787d43
kevin <kevinke386@gmail.com>
2024-05-23 09:53:41 +0300

    Updated

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name = "Kevin"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+    result = 45 + 55
+    print(result)
 
 def halve(number):
-    pass
+    return number/2
+    result = 2000/2
+    print(result)
